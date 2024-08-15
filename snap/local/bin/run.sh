0fdd8dc26578cb27e1902db4dfe374b10b5acb09
JP Meijers <jp.meijers@canonical.com>
2024-08-15 13:22:27 +0200

    More aggressive killing of child docker container

--- snap/local/bin/run.sh
+++ snap/local/bin/run.sh
@@ -3,20 +3,29 @@
 container_name="$SNAP_INSTANCE_NAME"
 image_name="xoseperez/basicstation:$DOCKER_IMAGE_TAG"
 
-# Kill contianer if it already exists
-docker rm "$container_name" || true
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  docker stop "$container_name"
+}
 
-if [ ! "$(docker ps --all --quiet --filter name="$container_name")" ]; then
-    echo "Container does not exist. Creating ..."
-    docker create \
-        --name "$container_name" \
-        --log-driver none \
-        --env-file "$SNAP_COMMON/conf.env" \
-        --device=/dev/spidev0.0:/dev/spidev0.0 \
-        --device=/dev/gpiochip0:/dev/gpiochip0 \
-        --device=/dev/gpiochip4:/dev/gpiochip4 \
-        "$image_name"
-fi
+_kill() {
+  echo "Caught SIGKILL signal!"
+  kill -9 "$child"
+}
+
+trap _term SIGTERM
+trap _kill SIGKILL
 
 echo "Starting container ..."
-docker start --attach "$container_name"
+docker run --interactive --rm \
+    --name "$container_name" \
+    --log-driver none \
+    --env-file "$SNAP_COMMON/conf.env" \
+    --device=/dev/spidev0.0:/dev/spidev0.0 \
+    --device=/dev/gpiochip0:/dev/gpiochip0 \
+    --device=/dev/gpiochip4:/dev/gpiochip4 \
+    "$image_name" \
+    start &
+
+child=$!
+wait
