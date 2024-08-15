25df1758e4c35c77092a6bec2665f3bfb4e17194
JP Meijers <jp.meijers@canonical.com>
2024-08-15 11:31:09 +0200

    Add gpio devices to docker container

--- snap/local/bin/run.sh
+++ snap/local/bin/run.sh
@@ -6,15 +6,17 @@ image_name="xoseperez/basicstation:$DOCKER_IMAGE_TAG"
 # Kill contianer if it already exists
 docker rm "$container_name" || true
 
-#if [ ! "$(docker ps --all --quiet --filter name="$container_name")" ]; then
+if [ ! "$(docker ps --all --quiet --filter name="$container_name")" ]; then
     echo "Container does not exist. Creating ..."
     docker create \
-        --device=/dev/spidev0.0:/dev/spidev0.0 \
         --name "$container_name" \
         --log-driver none \
         --env-file "$SNAP_COMMON/conf.env" \
+        --device=/dev/spidev0.0:/dev/spidev0.0 \
+        --device=/dev/gpiochip0:/dev/gpiochip0 \
+        --device=/dev/gpiochip4:/dev/gpiochip4 \
         "$image_name"
-#fi
+fi
 
 echo "Starting container ..."
 docker start --attach "$container_name"
