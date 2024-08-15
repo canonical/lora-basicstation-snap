eb59912d044a8608ff5645a7fc9cfecabce37eb9
JP Meijers <jp.meijers@canonical.com>
2024-08-15 16:19:31 +0200

    Add stop command to stop and remove the container

--- snap/local/bin/run.sh
+++ snap/local/bin/run.sh
@@ -3,29 +3,15 @@
 container_name="$SNAP_INSTANCE_NAME"
 image_name="xoseperez/basicstation:$DOCKER_IMAGE_TAG"
 
-_term() { 
-  echo "Caught SIGTERM signal!" 
-  docker stop "$container_name"
-}
-
-_kill() {
-  echo "Caught SIGKILL signal!"
-  kill -9 "$child"
-}
-
-trap _term SIGTERM
-trap _kill SIGKILL
+echo "Creating container ..."
+docker create \
+  --name "$container_name" \
+  --log-driver none \
+  --env-file "$SNAP_COMMON/conf.env" \
+  --device=/dev/spidev0.0:/dev/spidev0.0 \
+  --device=/dev/gpiochip0:/dev/gpiochip0 \
+  --device=/dev/gpiochip4:/dev/gpiochip4 \
+  "$image_name"
 
 echo "Starting container ..."
-docker run --interactive --rm \
-    --name "$container_name" \
-    --log-driver none \
-    --env-file "$SNAP_COMMON/conf.env" \
-    --device=/dev/spidev0.0:/dev/spidev0.0 \
-    --device=/dev/gpiochip0:/dev/gpiochip0 \
-    --device=/dev/gpiochip4:/dev/gpiochip4 \
-    "$image_name" \
-    start &
-
-child=$!
-wait
+docker start --attach "$container_name"
---
+++ snap/local/bin/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eu
+
+# When the snap is stopped or restarted, we always stop and remove the container.
+# This is so that it gets recreated with the latest config.env file when it starts.
+
+container_name="$SNAP_INSTANCE_NAME"
+
+docker stop "$container_name"
+docker rm -f "$container_name"
